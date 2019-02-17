4dac1af28e8559db6300b9d1ce941077cf58e2af
lNhatHox1 <30343545+lNhatHox1@users.noreply.github.com>
2019-02-17 20:43:47 +0700

    Update and rename lambdards.py to Monit-get-alert.py

--- Monit-get-alert.py
+++ Monit-get-alert.py
@@ -8,7 +8,7 @@ import datetime
 
 REGION = 'us-east-1'
 
-rds_host  = "appychip.cqjqjnza85a5.us-east-1.rds.amazonaws.com"
+rds_host  = "appychip.cae99tn0ffoc.us-east-1.rds.amazonaws.com"
 name = "appychip"
 password = "12345678"
 db_name = "appychip"
@@ -21,15 +21,18 @@ def main(event,context):
     conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
     result = []
     with conn.cursor() as cur:
-        cur.execute("select ttime,id,aname,type,urgency from test where aname = 'Testing stage 4'")
+        cur.execute("select ttime,id,name,type,urgency,host from test ")
         conn.commit()
         cur.close()
         for row in cur:
-            result.append({'ttime': row[0], 'id': row[1], 'aname': row[2],'type': row[3], 'urgency': row[4]})
+            result.append({'ttime': row[0], 'id': row[1], 'name': row[2],'type': row[3], 'urgency': row[4],'host': row[5]})
     final = json.dumps(result, default = myconverter)
     new = final.replace('\"','')
     return (new)
     
+
+
+
 
 
 
