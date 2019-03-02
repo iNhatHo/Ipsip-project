018220371df3f10709ae8b233ec4eca93b3f4a37
lNhatHox1 <30343545+lNhatHox1@users.noreply.github.com>
2019-03-02 18:21:26 +0700

    SQL
    + Creating new table for second request
    + Upload old practice code from december-2018

--- Create_table.sql
+++ Create_table.sql
@@ -8,4 +8,26 @@ Create table testalert (
     rtime datetime,
     htime varchar(5),
     primary key (ID),
-)
+    INDEX name_host (name,host),
+    CONSTRAINT testalert_testalertname_name FOREIGN KEY (name) REFERENCES testalertname (name),
+    CONSTRAINT testalert_testalertname_host FOREIGN KEY (host) REFERENCES testalertname (host)
+);
+Create table testperson (
+	ID int not null auto_increment,
+    name varchar(20),
+    primary key (ID),
+    Index name (name)
+);
+Create table testalertname (
+	ID int not null auto_increment,
+    name varchar(50),
+    host varchar(50),
+    normalinfo varchar(150),
+    newinfo varchar(150),
+    timein datetime,
+    timeout datetime,
+    personname varchar(20),
+    primary key (ID),
+    INDEX name_host (name,host),
+    CONSTRAINT testperson_testalertname FOREIGN KEY (personname) REFERENCES testperson (name)
+    )
