54a13c3b037621086a21be69ad1bd643d508f2f4
lNhatHox1 <30343545+lNhatHox1@users.noreply.github.com>
2019-03-07 15:27:35 +0700

    3/7/2019

--- SQL/Create_table.sql
+++ SQL/Create_table.sql
@@ -1,33 +1,50 @@
-Create table testalert (
+Create table alert (
 	ID int not null auto_increment,
     name varchar(50),
     host varchar(50),
-    urgency varchar(15),
+    tooltouse varchar(150),
+	urgency varchar(15),
+    primary key (ID)
+);
+Create table alerttrigger (
+	ID int not null auto_increment,
+    name varchar(50),
+    host varchar(50),
+	urgency varchar(15),
     type varchar(15),
     ttime datetime,
     rtime datetime,
     htime varchar(5),
-    primary key (ID),
-    INDEX name_host (name,host),
-    CONSTRAINT testalert_testalertname_name FOREIGN KEY (name) REFERENCES testalertname (name),
-    CONSTRAINT testalert_testalertname_host FOREIGN KEY (host) REFERENCES testalertname (host)
+    primary key (ID)
 );
-Create table testperson (
-	ID int not null auto_increment,
-    name varchar(20),
-    primary key (ID),
-    Index name (name)
-);
-Create table testalertname (
+Create table alertinfo (
 	ID int not null auto_increment,
     name varchar(50),
     host varchar(50),
-    normalinfo varchar(150),
-    newinfo varchar(150),
+    info varchar(150),
     timein datetime,
-    timeout datetime,
-    personname varchar(20),
-    primary key (ID),
-    INDEX name_host (name,host),
-    CONSTRAINT testperson_testalertname FOREIGN KEY (personname) REFERENCES testperson (name)
-    )
+    person varchar(20),
+    type varchar(15),
+    primary key (ID)
+);
+Create table  team (
+	ID int not null auto_increment,
+    name varchar(50),
+    primary key(ID)
+);
+Create table urgencytype (
+	ID int not null auto_increment,
+    urgency varchar(20),
+    primary key (ID)
+);
+
+Create index UT_urgency on urgencytype (urgency);
+Create index A_name_host on alert (name,host);
+Create index T_person_name on team (name);
+
+ALTER TABLE alert add constraint FK_alert_urgencytype foreign key (urgency) references urgencytype (urgency);
+ALTER TABLE alerttrigger add constraint FK_alerttrigger_alert_name foreign key (name,host) references alert (name,host);
+ALTER TABLE alerttrigger add constraint FK_alerttrigger_alert_urgency foreign key (urgency) references alert (urgency);
+ALTER TABLE alertinfo add constraint FK_alertinfo_alert foreign key (name,host) references alert (name,host);
+ALTER TABLE alertinfo add constraint FK_alertinfo_team foreign key (person) references team (name)
+
